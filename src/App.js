c4a499159df62b0b3ba89f557df44dcc00a9e79e
Divya Mishra <mishradivyagkp1999@gmail.com>
2024-03-31 13:12:29 +0530

    check for token

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Login } from './components/Login';
+import { useEffect } from 'react';
 
 function App() {
+  
+  useEffect(()=>{
+
+    const hash = window.location.hash;
+    if(hash){
+    const token =  hash.substring(1).split("&")[0].split("=")[1];
+    
+    if(token)
+    {
+      console.log(token);
+    }
+    
+    }
+
+  },[token,dispatch])
+   
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {token? "": <Login/>}
     </div>
+    </>
   );
 }
 
